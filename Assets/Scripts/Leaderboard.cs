c7f2182bac9bf05be1af77e2de48b17820f8fb2d
Francis <fwang36@gmail.com>
2023-06-18 13:57:38 -0400

    add load leaderboard function

---
+++ Assets/Scripts/Leaderboard.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leaderboard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    string leaderboardKey = "ADescriptiveKey_ThatYouSetInTheConsole";
+    int count = 50;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnLoadLeaderboard() 
+    {
+
+
+    LootLockerSDKManager.GetScoreList(leaderboardKey, count, 0, (response) =>
+{
+    if (response.statusCode == 200) {
+        Debug.Log("Successful");
+    } else {
+        Debug.Log("failed: " + response.Error);
+    }
+});
+    }
+}
