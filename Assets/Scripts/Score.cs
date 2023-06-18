25e8a3fe977212f3df640053416f6dcc1b1335b6
KobbyBawuah <Bawuah10@hotmail.com>
2023-06-18 00:25:57 -0400

    score feature commit

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
+using Sentry;
+using System;
 
 public class Score : MonoBehaviour
 {
-    public int score;
+    [FormerlySerializedAs("score")]
+    public int scoreAwarded;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,8 +19,21 @@ public class Score : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // score = score + 50;
-        // print(score);
+
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        var newScore = PlayerPrefs.GetInt("totalScore")+ scoreAwarded;
+        PlayerPrefs.SetInt("totalScore", newScore);
+        PlayerPrefs.Save();
+        print("Your score is:");
+        print(newScore);
+
+        // SentrySdk.CaptureMessage("Something went wrong");
+        // throw new Exception("Kobby test error from score feature");
+
     }
 
 }
+
